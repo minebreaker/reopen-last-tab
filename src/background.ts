fd4cae4e50a45eab6f1814b2cdcbe126505821d3
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-09-19 23:16:43 +0900

    init

---
+++ src/background.ts
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.browserAction.onClicked.addListener(() => {
+        chrome.sessions.getRecentlyClosed({maxResults: 1}, ([session]) => {
+            if (!session || !session.tab) return
+
+            chrome.sessions.restore(session.tab.sessionId)
+        })
+    })
+})
