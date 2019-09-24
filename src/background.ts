4771b691a880475dcf243d76684f39a6b794b604
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-09-24 23:34:35 +0900

    persistent: true

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,7 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.browserAction.onClicked.addListener(() => {
-        chrome.sessions.getRecentlyClosed({maxResults: 1}, ([session]) => {
-            if (!session || !session.tab) return
+chrome.browserAction.onClicked.addListener( () => {
+    chrome.sessions.getRecentlyClosed( { maxResults: 1 }, ( [session] ) => {
+        if ( !session || !session.tab ) return
 
-            chrome.sessions.restore(session.tab.sessionId)
-        })
-    })
-})
+        chrome.sessions.restore( session.tab.sessionId )
+    } )
+} )
